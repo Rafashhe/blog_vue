e0094a02cbcf2a9187258295e84996d152d226d1
Rafashhe <cwb.rafaella@gmail.com>
2023-05-26 09:01:04 -0300

    fix base

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-  base: process.env.NODE_ENV === 'production' ? '/Personal_Page/' : '/'
 })
+base: process.env.NODE_ENV === 'production' ? '/Personal_Page/' : '/'
