f625783014bdcdc07e176b9535504299185b4783
Rafashhe <cwb.rafaella@gmail.com>
2023-05-26 09:16:42 -0300

    att base

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/blog_vue/' : '/'
 })
-base: process.env.NODE_ENV === 'production' ? '/Personal_Page/' : '/'
